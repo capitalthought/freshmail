77983446db3197afbf256611fb2d66370885f3d5
David Giesberg <git@davidgiesberg.net>
2010-11-26 16:31:04 -0600

    Initial commit - Rails 3 + devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freshmail::Application.config.secret_token = 'b747307b52f32e1b8d5f6a818e6413777d208cc3e912680e6078d92eeb5129859f97ff7539b1e1b57f18a814fe1d41fee513e8ceeb295aec2ce595e9c95beb47'
