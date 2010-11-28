0cafb206f88e31cf4ce71b53b83b37e674149b1d
David Giesberg <git@davidgiesberg.net>
2010-11-28 16:41:05 -0600

    Add database seed user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+user = User.create( 
+  :email                 => 'test@test.com', 
+  :password              => 'password',
+  :password_confirmation => 'password'
+) 
